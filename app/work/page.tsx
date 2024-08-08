7b1c7458bc9f5366164d8f298c506da9de3c90e6
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-08-09 03:26:11 +0530

    Work Page Content Added

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -13,11 +13,30 @@ export const metadata: Metadata = generateMetadata({
 
 const WorkPage: React.FC = () => {
   return (
-    <main className="grid min-h-screen flex-1 place-content-center">
-      <div className="mx-auto max-w-[580px] px-4 md:px-0">
-        <h1 className="mb-7 text-5xl font-semibold leading-snug text-neutral-800 dark:text-neutral-300">
-          <span className="text-neutral-300 dark:text-neutral-700">Soon.</span>
+    <main className="flex min-h-screen flex-1 items-center justify-center">
+      <div className="mx-auto w-full max-w-[580px] px-4 md:px-0">
+        <h1 className="mb-20 text-3xl font-semibold leading-snug text-neutral-800 dark:text-neutral-300">
+          Work <br />
+          <span className="text-neutral-300 dark:text-neutral-700">
+            Experience.
+          </span>
         </h1>
+        <div className="space-y-2 text-neutral-800 dark:text-neutral-400">
+          <div className="flex items-center gap-2">
+            <p>Jupiter Money</p>
+            <div className="w-20 flex-1 border-t border-dashed border-neutral-300 dark:border-neutral-800" />
+            <p className="text-neutral-300 dark:text-neutral-700">
+              2022 - 2024
+            </p>
+          </div>
+          <div className="flex items-center gap-2">
+            <p>ZS Associates</p>
+            <div className="w-20 flex-1 border-t border-dashed border-neutral-300 dark:border-neutral-800" />
+            <p className="text-neutral-300 dark:text-neutral-700">
+              2021 - 2022
+            </p>
+          </div>
+        </div>
       </div>
     </main>
   )
