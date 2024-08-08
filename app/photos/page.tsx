7034dfa8963adace4d7b745da0187d233fda3ac3
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-08-09 03:04:55 +0530

    Photos Page Added

--- app/photos/page.tsx
+++ app/photos/page.tsx
@@ -2,6 +2,8 @@ import * as React from "react"
 
 import { type Metadata } from "next"
 
+import Image from "next/image"
+
 import generateMetadata from "utils/seo"
 
 export const metadata: Metadata = generateMetadata({
@@ -10,16 +12,58 @@ export const metadata: Metadata = generateMetadata({
   description: "Stunning pictures captured by Vatsal Dhameliya",
 })
 
-const WorkPage: React.FC = () => {
+const photos = [
+  "IMG_0620.jpeg",
+  "IMG_0630.jpeg",
+  "IMG_1691.jpeg",
+  "IMG_2563.jpeg",
+  "IMG_2571.jpeg",
+  "IMG_2579.jpeg",
+  "IMG_5398.jpeg",
+  "IMG_5404.jpeg",
+  "IMG_5472.jpeg",
+  "IMG_5515.jpeg",
+  "IMG_6162.jpeg",
+  "IMG_6284.jpeg",
+  // "IMG_6288.jpeg",
+  // "IMG_6406.jpeg",
+  "IMG_6422.jpeg",
+  "IMG_6441.jpeg",
+  // "IMG_6826.jpeg",
+  "IMG_6864.jpeg",
+  // "IMG_8434.jpeg",
+  "IMG_8490.jpeg",
+  "IMG_8513.jpeg",
+  "IMG_8633.jpeg",
+  "IMG_8798.jpeg",
+]
+
+const PhotosPage: React.FC = () => {
   return (
-    <main className="grid min-h-screen flex-1 place-content-center">
-      <div className="mx-auto max-w-[580px] px-4 md:px-0">
-        <h1 className="mb-7 text-5xl font-semibold leading-snug text-neutral-800 dark:text-neutral-300">
-          <span className="text-neutral-300 dark:text-neutral-700">Soon.</span>
+    <main className="grid h-screen flex-1 overflow-scroll">
+      <div className="mx-auto max-w-[980px] px-4 md:px-0">
+        <h1 className="mb-7 mt-24 text-3xl font-semibold leading-snug text-neutral-800 dark:text-neutral-300">
+          Moments <br />
+          <span className="text-neutral-300 dark:text-neutral-700">
+            & Pictures
+          </span>
         </h1>
+        <div className="mb-24 grid grid-cols-4 gap-5">
+          {photos.map((photo, index) => (
+            <div key={index} className="rounded-3xl bg-white p-2.5">
+              <Image
+                src={`/photos/${photo}`}
+                alt={photo}
+                width={300}
+                height={533}
+                className="rounded-2xl"
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </main>
   )
 }
 
-export default WorkPage
+export default PhotosPage
