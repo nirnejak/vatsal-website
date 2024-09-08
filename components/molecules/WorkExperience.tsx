81fd7ae382273ec51d006cf9b7bb5f6f574b29ff
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-09-09 00:25:05 +0530

    WorkExperience: single h1

--- components/molecules/WorkExperience.tsx
+++ components/molecules/WorkExperience.tsx
@@ -8,24 +8,26 @@ import { BASE_TRANSITION } from "@/utils/animation"
 const WorkExperience: React.FC = () => {
   return (
     <div className="mx-auto w-full max-w-[580px] px-4 md:px-0">
-      <motion.h1
-        initial={{ translateY: 10, opacity: 0 }}
-        animate={{ translateY: 0, opacity: 1 }}
-        transition={{ delay: 0, ...BASE_TRANSITION }}
-        className="text-3xl font-semibold leading-snug text-neutral-800 dark:text-neutral-300"
-      >
-        Work <br />
-      </motion.h1>
-      <motion.h1
-        initial={{ translateY: 10, opacity: 0 }}
-        animate={{ translateY: 0, opacity: 1 }}
-        transition={{ delay: 0.1, ...BASE_TRANSITION }}
-        className="mb-20 text-3xl font-semibold leading-snug text-neutral-800 dark:text-neutral-300"
-      >
-        <span className="text-neutral-400 dark:text-neutral-600">
-          Experience.
-        </span>
-      </motion.h1>
+      <h1>
+        <motion.span
+          initial={{ translateY: 10, opacity: 0 }}
+          animate={{ translateY: 0, opacity: 1 }}
+          transition={{ delay: 0, ...BASE_TRANSITION }}
+          className="flex text-3xl font-semibold leading-snug text-neutral-800 dark:text-neutral-300"
+        >
+          Work <br />
+        </motion.span>
+        <motion.span
+          initial={{ translateY: 10, opacity: 0 }}
+          animate={{ translateY: 0, opacity: 1 }}
+          transition={{ delay: 0.1, ...BASE_TRANSITION }}
+          className="mb-20 flex text-3xl font-semibold leading-snug text-neutral-800 dark:text-neutral-300"
+        >
+          <span className="text-neutral-400 dark:text-neutral-600">
+            Experience.
+          </span>
+        </motion.span>
+      </h1>
       <motion.div
         initial={{ translateY: 10, opacity: 0 }}
         animate={{ translateY: 0, opacity: 1 }}
