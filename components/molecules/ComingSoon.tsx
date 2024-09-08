bcf23f8510a2b5bbe42952f9fd7b40af53c4c4cf
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-09-09 00:24:36 +0530

    Coming Soon: single h1

--- components/molecules/ComingSoon.tsx
+++ components/molecules/ComingSoon.tsx
@@ -8,22 +8,24 @@ import { BASE_TRANSITION } from "@/utils/animation"
 const ComingSoon: React.FC = () => {
   return (
     <div className="mx-auto w-full max-w-[580px] px-4 md:px-0">
-      <motion.h1
-        initial={{ translateY: 10, opacity: 0 }}
-        animate={{ translateY: 0, opacity: 1 }}
-        transition={{ delay: 0, ...BASE_TRANSITION }}
-        className="text-3xl font-semibold leading-snug text-neutral-800 dark:text-neutral-300"
-      >
-        Coming <br />
-      </motion.h1>
-      <motion.h1
-        initial={{ translateY: 10, opacity: 0 }}
-        animate={{ translateY: 0, opacity: 1 }}
-        transition={{ delay: 0.1, ...BASE_TRANSITION }}
-        className="text-3xl font-semibold leading-snug text-neutral-800 dark:text-neutral-300"
-      >
-        <span className="text-neutral-400 dark:text-neutral-600">Soon.</span>
-      </motion.h1>
+      <h1>
+        <motion.span
+          initial={{ translateY: 10, opacity: 0 }}
+          animate={{ translateY: 0, opacity: 1 }}
+          transition={{ delay: 0, ...BASE_TRANSITION }}
+          className="flex text-3xl font-semibold leading-snug text-neutral-800 dark:text-neutral-300"
+        >
+          Coming <br />
+        </motion.span>
+        <motion.span
+          initial={{ translateY: 10, opacity: 0 }}
+          animate={{ translateY: 0, opacity: 1 }}
+          transition={{ delay: 0.1, ...BASE_TRANSITION }}
+          className="flex text-3xl font-semibold leading-snug text-neutral-800 dark:text-neutral-300"
+        >
+          <span className="text-neutral-400 dark:text-neutral-600">Soon.</span>
+        </motion.span>
+      </h1>
     </div>
   )
 }
