b72f6c66d7e51dc15a9ce79e4a321f729dfa5e6f
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-11-13 16:20:55 +0530

    math and latex setup

--- utils/markdownToHtml.ts
+++ utils/markdownToHtml.ts
@@ -1,7 +1,9 @@
 import rehypeShiki from "@shikijs/rehype"
 import rehypeFormat from "rehype-format"
+import rehypeMathjax from "rehype-mathjax"
 import rehypeRaw from "rehype-raw"
 import rehypeStringify from "rehype-stringify"
+import math from "remark-math"
 import remarkParse from "remark-parse"
 import remarkRehype from "remark-rehype"
 import { unified } from "unified"
@@ -12,12 +14,15 @@ export default async function markdownToHtml(
   const result = await unified()
     .use(remarkParse)
     .use(remarkRehype, { allowDangerousHtml: true })
+    .use(math)
     // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
     .use(rehypeShiki, {
+      inline: "tailing-curly-colon",
       theme: "vitesse-black",
     })
     .use(rehypeRaw)
     .use(rehypeFormat)
+    .use(rehypeMathjax)
     .use(rehypeStringify)
     .process(markdown)
 
