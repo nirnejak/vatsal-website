f63317c65255e5a675d20e8fed9a84708bf4992c
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-11-13 20:03:27 +0530

    Radix tooltip removed

--- components/molecules/Navbar.tsx
+++ components/molecules/Navbar.tsx
@@ -1,7 +1,6 @@
 "use client"
 import * as React from "react"
 
-import * as Tooltip from "@radix-ui/react-tooltip"
 import { DoubleCheck, Home, Image as ImageIcon, Newspaper } from "akar-icons"
 import * as motion from "framer-motion/client"
 import Link from "next/link"
@@ -48,33 +47,18 @@ const Navbar: React.FC = () => {
         transition={{ delay: 0.5 }}
         className="flex items-center justify-center"
       >
-        <Tooltip.Provider delayDuration={200}>
-          {links.map((link) => (
-            <Tooltip.Root key={link.text}>
-              <Tooltip.Trigger asChild>
-                <Link
-                  href={link.href}
-                  className={classNames(
-                    pathname === link.href ? "bg-neutral-800" : "",
-                    "inline-flex items-center gap-2 rounded-3xl px-2.5 py-1.5 md:px-3.5 md:py-2 transition-colors text-neutral-200 dark:text-neutral-400 hover:bg-neutral-800"
-                  )}
-                >
-                  {link.value} {link.text}
-                </Link>
-              </Tooltip.Trigger>
-              <Tooltip.Portal>
-                <Tooltip.Content
-                  className="select-none rounded-md bg-neutral-800 px-3.5 py-2.5 text-sm leading-none text-neutral-300 will-change-[transform,opacity] data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade"
-                  sideOffset={5}
-                  side="top"
-                >
-                  {link.text}
-                  <Tooltip.Arrow className="fill-neutral-800" />
-                </Tooltip.Content>
-              </Tooltip.Portal>
-            </Tooltip.Root>
-          ))}
-        </Tooltip.Provider>
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            href={link.href}
+            className={classNames(
+              pathname === link.href ? "bg-neutral-800" : "",
+              "flex items-center gap-2 rounded-3xl px-2.5 py-1.5 md:px-3.5 md:py-2 transition-colors text-neutral-200 dark:text-neutral-400 hover:bg-neutral-800"
+            )}
+          >
+            {link.value} {link.text}
+          </Link>
+        ))}
       </motion.div>
     </motion.nav>
   )
