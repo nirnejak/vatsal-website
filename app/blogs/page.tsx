377c0bb9fd2725b75e138edb8d461dc5418fb67c
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-11-13 13:03:16 +0530

    Blog List page

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -2,19 +2,80 @@ import * as React from "react"
 
 import { type Metadata } from "next"
 
-import ComingSoon from "@/components/molecules/ComingSoon"
+import * as motion from "framer-motion/client"
+import Link from "next/link"
+
+import ComingSoon from "components/molecules/ComingSoon"
+import { BASE_TRANSITION } from "utils/animation"
+import { getAllBlogs } from "utils/blog"
 import generateMetadata from "utils/seo"
 
 export const metadata: Metadata = generateMetadata({
   path: "/blogs/",
   title: "Blogs | Vatsal Dhameliya - AI Scientist",
-  description: "Blogs on AI and tech writted by Vatsal Dhameliya",
+  description: "Blogs and articles on AI and tech written by Vatsal Dhameliya",
 })
 
 const BlogsPage: React.FC = () => {
+  const blogs = getAllBlogs([
+    "active",
+    "image",
+    "date",
+    "slug",
+    "title",
+    "description",
+  ])
+
   return (
     <main className="flex min-h-screen items-center justify-center">
-      <ComingSoon />
+      {blogs.length === 0 ? (
+        <ComingSoon />
+      ) : (
+        <div className="mx-auto w-full max-w-[580px] px-4 md:px-0">
+          <h1>
+            <motion.span
+              initial={{ translateY: 10, opacity: 0 }}
+              animate={{ translateY: 0, opacity: 1 }}
+              transition={{ delay: 0, ...BASE_TRANSITION }}
+              className="flex text-3xl font-semibold leading-snug text-neutral-800 dark:text-neutral-300"
+            >
+              Blogs <br />
+            </motion.span>
+            <motion.span
+              initial={{ translateY: 10, opacity: 0 }}
+              animate={{ translateY: 0, opacity: 1 }}
+              transition={{ delay: 0.1, ...BASE_TRANSITION }}
+              className="mb-20 flex text-3xl font-semibold leading-snug text-neutral-800 dark:text-neutral-300"
+            >
+              <span className="text-neutral-400 dark:text-neutral-600">
+                & Articles.
+              </span>
+            </motion.span>
+          </h1>
+          <div className="space-y-2 text-neutral-800 dark:text-neutral-400">
+            {blogs.map((blog, index) => (
+              <motion.div
+                key={index}
+                initial={{ translateY: 10, opacity: 0 }}
+                animate={{ translateY: 0, opacity: 1 }}
+                transition={{
+                  delay: 0.2 + 0.1 * (index + 1),
+                  ...BASE_TRANSITION,
+                }}
+              >
+                <Link
+                  href={`/blogs/${blog.slug}`}
+                  className="flex items-center gap-2"
+                >
+                  <p className="font-medium">{blog.title}</p>
+                  <div className="flex-1 border-t border-dashed border-neutral-300 dark:border-neutral-800" />
+                  <p>{blog.date}</p>
+                </Link>
+              </motion.div>
+            ))}
+          </div>
+        </div>
+      )}
     </main>
   )
 }
