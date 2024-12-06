71374a2b2b8579fb6b20e5b266c24ded79cfea42
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-12-06 13:44:27 +0530

    Photos: imports and list moved to a util

--- app/photos/page.tsx
+++ app/photos/page.tsx
@@ -5,30 +5,8 @@ import { type Metadata } from "next"
 import * as motion from "framer-motion/client"
 import Image from "next/image"
 
-import Photo1 from "assets/photos/IMG_0620.jpeg"
-import Photo2 from "assets/photos/IMG_0630.jpeg"
-import Photo3 from "assets/photos/IMG_1691.jpeg"
-import Photo4 from "assets/photos/IMG_2563.jpeg"
-import Photo5 from "assets/photos/IMG_2571.jpeg"
-import Photo6 from "assets/photos/IMG_2579.jpeg"
-import Photo7 from "assets/photos/IMG_5398.jpeg"
-import Photo8 from "assets/photos/IMG_5404.jpeg"
-import Photo9 from "assets/photos/IMG_5472.jpeg"
-import Photo10 from "assets/photos/IMG_5515.jpeg"
-import Photo11 from "assets/photos/IMG_6162.jpeg"
-import Photo12 from "assets/photos/IMG_6284.jpeg"
-import Photo13 from "assets/photos/IMG_6288.jpeg"
-import Photo14 from "assets/photos/IMG_6406.jpeg"
-import Photo15 from "assets/photos/IMG_6422.jpeg"
-import Photo16 from "assets/photos/IMG_6441.jpeg"
-import Photo17 from "assets/photos/IMG_6826.jpeg"
-import Photo18 from "assets/photos/IMG_6864.jpeg"
-import Photo19 from "assets/photos/IMG_8434.jpeg"
-import Photo20 from "assets/photos/IMG_8490.jpeg"
-import Photo21 from "assets/photos/IMG_8513.jpeg"
-import Photo22 from "assets/photos/IMG_8633.jpeg"
-import Photo23 from "assets/photos/IMG_8798.jpeg"
 import { BASE_TRANSITION } from "utils/animation"
+import { photos } from "utils/photos"
 import generateMetadata from "utils/seo"
 
 export const metadata: Metadata = generateMetadata({
@@ -90,29 +68,3 @@ const PhotosPage: React.FC = () => {
 }
 
 export default PhotosPage
-
-const photos = [
-  Photo1,
-  Photo2,
-  Photo3,
-  Photo4,
-  Photo5,
-  Photo6,
-  Photo7,
-  Photo8,
-  Photo9,
-  Photo10,
-  Photo11,
-  Photo12,
-  Photo13,
-  Photo14,
-  Photo15,
-  Photo16,
-  Photo17,
-  Photo18,
-  Photo19,
-  Photo20,
-  Photo21,
-  Photo22,
-  Photo23,
-]
