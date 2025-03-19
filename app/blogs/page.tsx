2b2f4177796c577215867cf8adce7e6bc7564f20
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-03-20 02:53:03 +0530

    layout fixes

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -96,38 +96,32 @@ const blogs = [
 
 const BlogsPage: React.FC = () => {
   return (
-    <main className="flex min-h-dvh items-center justify-center">
-      {blogs.length === 0 ? (
-        <ComingSoon />
-      ) : (
-        <div className="mx-auto w-full max-w-[580px] px-4 md:px-0">
-          <h1 className="mb-8 mt-12 text-3xl font-semibold leading-snug tracking-tight md:mb-20 md:mt-0">
-            <span className="flex text-neutral-800 dark:text-neutral-300">
-              Blogs <br />
-            </span>
-            <span className="flex text-neutral-400 dark:text-neutral-600">
-              & Articles.
-            </span>
-          </h1>
-          <div className="space-y-2.5 text-neutral-800 dark:text-neutral-400">
-            {blogs.map((blog, index) => (
-              <div key={index}>
-                <Link
-                  href={`/blogs/${blog.slug}`}
-                  className="group flex items-center gap-2"
-                >
-                  <p className="font-medium">{blog.title}</p>
-                  <div className="flex-1 border-t border-dashed border-neutral-300 dark:border-neutral-800" />
-                  <ArrowRight
-                    size={15}
-                    className="transition-transform group-hover:translate-x-1"
-                  />
-                </Link>
-              </div>
-            ))}
+    <main className="mx-auto w-full max-w-[580px] px-4 md:px-0">
+      <h1 className="mb-8 mt-12 text-3xl font-semibold leading-snug tracking-tight md:mb-12">
+        <span className="flex text-neutral-800 dark:text-neutral-300">
+          Blogs <br />
+        </span>
+        <span className="flex text-neutral-400 dark:text-neutral-600">
+          & Articles.
+        </span>
+      </h1>
+      <div className="space-y-2.5 text-neutral-800 dark:text-neutral-400 mb-24">
+        {blogs.map((blog, index) => (
+          <div key={index}>
+            <Link
+              href={`/blogs/${blog.slug}`}
+              className="group flex items-center gap-2"
+            >
+              <p className="font-medium">{blog.title}</p>
+              <div className="flex-1 border-t border-dashed border-neutral-300 dark:border-neutral-800" />
+              <ArrowRight
+                size={15}
+                className="transition-transform group-hover:translate-x-1"
+              />
+            </Link>
           </div>
-        </div>
-      )}
+        ))}
+      </div>
     </main>
   )
 }
--- app/photos/page.tsx
+++ app/photos/page.tsx
@@ -14,33 +14,31 @@ export const metadata: Metadata = getMetadata({
 
 const PhotosPage: React.FC = () => {
   return (
-    <main className="flex min-h-dvh items-center justify-center">
-      <div className="mx-auto max-w-[980px] px-4 md:px-0">
-        <h1 className="mb-8 mt-12 text-3xl font-semibold leading-snug tracking-tight md:mb-20 md:mt-0">
-          <span className="flex text-neutral-800 dark:text-neutral-300">
-            Moments
-          </span>
-          <span className="flex text-neutral-400 dark:text-neutral-600">
-            & Pictures.
-          </span>
-        </h1>
-        <div className="mb-24 grid grid-cols-2 gap-3 md:grid-cols-3 md:gap-5 lg:grid-cols-4">
-          {photos.map((photo, index) => (
-            <div
-              key={index}
-              className="rounded-3xl bg-white p-2.5 shadow-lg dark:bg-neutral-100/10"
-            >
-              <Image
-                src={photo}
-                alt={photo.src}
-                width={300}
-                height={533}
-                placeholder="blur"
-                className="rounded-2xl"
-              />
-            </div>
-          ))}
-        </div>
+    <main className="mx-auto max-w-[980px] px-4 md:px-0">
+      <h1 className="mb-8 mt-16 text-3xl font-semibold leading-snug tracking-tight md:mb-12">
+        <span className="flex text-neutral-800 dark:text-neutral-300">
+          Moments
+        </span>
+        <span className="flex text-neutral-400 dark:text-neutral-600">
+          & Pictures.
+        </span>
+      </h1>
+      <div className="mb-24 grid grid-cols-2 gap-3 md:grid-cols-3 md:gap-5 lg:grid-cols-4">
+        {photos.map((photo, index) => (
+          <div
+            key={index}
+            className="rounded-3xl bg-white p-2.5 shadow-lg dark:bg-neutral-100/10"
+          >
+            <Image
+              src={photo}
+              alt={photo.src}
+              width={300}
+              height={533}
+              placeholder="blur"
+              className="rounded-2xl"
+            />
+          </div>
+        ))}
       </div>
     </main>
   )
